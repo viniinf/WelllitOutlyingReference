d9639020d91f915a0633937cdcb70dbb27e69b6b
viniinf <108034765+viniinf@users.noreply.github.com>
2023-06-25 22:24:07 +0000

    imagem aleatória página não encontrada - criar cadastro crud

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,23 @@ const LogoImage = styled('img')`
 
 // Componente da página "Página Não Encontrada"
 const NotFoundPage = () => {
+  const imageUrl = 'https://picsum.photos/1200/800';
+
+  const styles = {
+    backgroundImage: `url(${imageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: '2rem',
+  };
+
   return (
-    <div>
+    <div style={styles}>
       <h2>Página Não Encontrada</h2>
       <p>A página que você está procurando não existe ou não está disponível</p>
     </div>
